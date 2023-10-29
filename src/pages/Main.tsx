84fb3fa6a782d1f26e05989b53c109d56bced3b2
Rodion <rodionserez@gmail.com>
2023-10-29 20:49:51 +0300

    feat: add error boundary

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -4,12 +4,26 @@ import { SearchContent } from '../components/searchContent/SearchContent';
 import { Button } from '../components/UI/Button/Button';
 
 export class Main extends Component {
+  state = { isErrorButtonClicked: false };
+
   render(): React.ReactNode {
+    if (this.state.isErrorButtonClicked)
+      throw new Error('Error button clicked');
+
     return (
       <main className="page-content">
         <SearchField />
         <SearchContent />
-        <Button className="error-button"> throw error </Button>
+        <Button
+          className="error-button"
+          onClick={() =>
+            this.setState({
+              isErrorButtonClicked: true,
+            })
+          }
+        >
+          throw error
+        </Button>
       </main>
     );
   }
