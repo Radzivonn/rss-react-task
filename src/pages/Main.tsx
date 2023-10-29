428718b1369478211dd0774056cc6cc8be387dde
Rodion <rodionserez@gmail.com>
2023-10-29 18:35:03 +0300

    feat: add layout of main page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+import { SearchField } from '../components/searchField/SearchField';
+import { SearchContent } from '../components/searchContent/SearchContent';
+import { Button } from '../components/UI/Button/Button';
 
 export class Main extends Component {
   render(): React.ReactNode {
-    return <></>;
+    return (
+      <main className="page-content">
+        <SearchField />
+        <SearchContent />
+        <Button className="error-button"> throw error </Button>
+      </main>
+    );
   }
 }
