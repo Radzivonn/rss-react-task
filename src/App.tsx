bdcdc3cd1ba3a24e4d0bd3f54ffcd02decd12afc
Rodion <rodionserez@gmail.com>
2023-10-29 18:35:41 +0300

    feat: import styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Layout } from './components/Layout';
+import './styles/App.scss';
 
 export class App extends Component {
   render(): React.ReactNode {
