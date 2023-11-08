b576bd612d2bb38dbd5fbdb831e54f9ef61b9122
Rodion <rodionserez@gmail.com>
2023-11-08 19:53:57 +0300

    fix: fix bug with incorrect redirect

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,21 +1,16 @@
-import React, { useEffect } from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { routes } from './routes';
 import { Main } from '../pages/main/Main';
 import { Details } from '../pages/details/Details';
 
 const AppRouter = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate('/1');
-  }, []);
-
   return (
     <Routes>
       <Route path={routes.main} element={<Main />}>
         <Route path={routes.details} element={<Details />} />
       </Route>
+      <Route path="*" element={<Navigate to="/1" replace />} />
     </Routes>
   );
 };
