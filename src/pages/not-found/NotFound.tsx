3bf97a1475057fd6498d53a81e0e1ee8680b8df2
Rodion <rodionserez@gmail.com>
2023-11-19 17:38:58 +0300

    feat: add not found page

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { NOT_FOUND_PAGE_MESSAGE } from '../../constants/constants';
 
 export const NotFound = () => {
-  return <h1> {NOT_FOUND_PAGE_MESSAGE} </h1>;
+  return <h1 data-testid="not-found-page"> {NOT_FOUND_PAGE_MESSAGE} </h1>;
 };
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { routes } from './routes';
 import { Main } from '../pages/main/Main';
 import { Details } from '../pages/details/Details';
+import { NotFound } from '../pages/not-found/NotFound';
 
 const AppRouter = () => {
   return (
@@ -10,8 +11,7 @@ const AppRouter = () => {
       <Route path={routes.main} element={<Main />}>
         <Route path={routes.details} element={<Details />} />
       </Route>
-      {/* <Route path="/*" element={<NotFound />} /> */}
-      <Route path="*" element={<Navigate to="/1" replace />} />
+      <Route path="/*" element={<NotFound />} />
     </Routes>
   );
 };
